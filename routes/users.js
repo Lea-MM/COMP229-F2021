1143f2bb1dc7f8247e9478cdb58970f9560a48d3
Lea-MM <leamagbalot@outlook.com>
2021-10-01 20:40:38 -0400

    added comments to ejs contentsagain

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('This is a user level site.');
 });
 
 module.exports = router;
